230204bd71619b61aee9b390933e6b050cc57a3b
tal0311 <tal.amit0311@gmail.com>
2023-06-18 09:23:10 +0300

    get msg list and by user id

--- api/msg/msg.service.js
+++ api/msg/msg.service.js
@@ -9,10 +9,12 @@ async function query(ownerId) {
     try {
         const collection = await dbService.getCollection('msg');
         const result = await collection.aggregate([
-            { $match: { ownerId } },
-            { $project: { history: 1 } },
+            { $project: { [`${ownerId}.history`]: 1 } }
         ]).toArray()
-        return result[0].history
+        // db.getCollection("msg").aggregate([
+        //     { $project: { "643d2a0f99553dc5ce88b861.history.643d2a0f99553dc5ce88b860": 1 } }
+        // ])
+        return result[0][ownerId].history
     } catch (error) {
 
     }
@@ -32,10 +34,12 @@ async function getByIdUserId(ownerId, userId) {
     try {
         const collection = await dbService.getCollection('msg');
         const result = collection.aggregate([
-            { $match: { ownerId } },
-            { $project: { [`history.${userId}`]: 1 } },
+            { $project: { [`${ownerId}.history.${userId}`]: 1 } }
 
         ]).toArray()
+        // db.getCollection("msg").aggregate([
+        //     { $project: { "643d2a0f99553dc5ce88b861.history.643d2a0f99553dc5ce88b860": 1 } }
+        // ])
         return result
     } catch (err) {
         logger.error(`while finding msg ${ownerId}`, err);
